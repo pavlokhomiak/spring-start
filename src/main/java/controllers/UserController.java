a524313206306d1c814ca1f2480763fd553c69b3
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-10-21 11:37:48 +0300

    Remove HelloController, remove Hellp.jsp, remove been for mapping to jsp

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -13,7 +13,6 @@ import service.UserService;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-
     private final UserService userService;
 
     public UserController(UserService userService) {
